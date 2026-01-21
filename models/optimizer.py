0a2848e1630cde0f22d66e097b032b4fc9083793
Jae-Hyun Park <benpak2015@gmail.com>
2026-01-21 21:26:30 +0900

    Fix LayerDecayOptimizer applying decay every step instead of once
    Layer decay should be applied once at initialization to set different
    learning rates for different layers, not multiplied every step which
    causes LR to exponentially decay to zero.
    
    Fixes #20

--- models/optimizer.py
+++ models/optimizer.py
@@ -5,11 +5,13 @@ class LayerDecayOptimizer:
         self.optimizer = optimizer
         self.layerwise_decay_rate = layerwise_decay_rate
         self.param_groups = optimizer.param_groups
-        
-    def step(self, *args, **kwargs):
+
+        # Apply layer-wise decay rate once at initialization
         for i, group in enumerate(self.optimizer.param_groups):
-            group['lr'] *= self.layerwise_decay_rate[i]
+            group['lr'] *= layerwise_decay_rate[i]
+
+    def step(self, *args, **kwargs):
         self.optimizer.step(*args, **kwargs)
-        
+
     def zero_grad(self, *args, **kwargs):
         self.optimizer.zero_grad(*args, **kwargs)
